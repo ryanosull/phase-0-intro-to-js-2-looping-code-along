dab46d610da6c7686209bd4a2a12b1d2ccc99c6c
Ryan <wrosullivan88@gmail.com>
2022-09-26 08:54:10 -0500

    commit tree 59538bf7360e1fc3fb176ab6663a47e91b15d057

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+/*function writeCards(name, event){
+    const newArray = []
+    for(let i = 0 ; i < name.length; i++){
+    newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+}
+*/
+//  
+function countDown(n){
+    while (n >= 0){
+    console.log(n--)
+    }
+}
+//
+function writeCards(name, event){
+    const newArray = []
+    for(let i = 0; i < name.length; i++)
+    newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    return newArray
+}
+
+///////////////////
+
+// function writeCards(names, party){
+
+//     const surprise = [];
+//     for (let i = 0; i < names.length; i++) {
+//     const newString = (`Thank you, ${names[i]}, for the wonderful ${party} gift!`)
+//     surprise.push(newString);
+    
+//     }
+//     return surprise;
+// }
